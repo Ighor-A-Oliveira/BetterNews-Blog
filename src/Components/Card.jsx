57424fdaf23bee873887853c074c9aed3fe42a62
Ighor Oliveira <ighor9991@gmail.com>
2024-12-08 13:43:14 -0300

    add a style

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -9,7 +9,7 @@ export default function Card({article}) {
     style={{ backgroundImage: `url(${article.image})` }}
     >
       <Link to={`artigo/${article.id}`}>
-        <div className="absolute h-[50%] w-full z-20 bottom-0 left-0 bg-gradient-to-b from-black/5 to-black/80 hover:from-black/20 hover:to-black/100 p-1 transition-shadow duration-300">
+        <div className="absolute h-[50%] w-full z-20 bottom-0 left-0 bg-gradient-to-b from-black/0 to-black/80 hover:from-black/20 hover:to-black/100 p-1 transition-shadow duration-500">
           <p className="text-white text-4xl font-sans text-left absolute bottom-[25px] left-[25px] right-[25px] text-wrap z-30">{article.title}</p>
         </div>
       </Link>
